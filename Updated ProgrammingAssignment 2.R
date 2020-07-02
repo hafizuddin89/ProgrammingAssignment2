c90db0b9439fe10179c2e58aa6f0b3573762f6be
hafizuddin89 <66362136+hafizuddin89@users.noreply.github.com>
2020-07-02 17:56:55 +0800

    Updated syntax to ProgrammingAsignment 2

---
+++ Updated ProgrammingAssignment 2.R
@@ -0,0 +1,26 @@
+### 1. make cache matrix create matrix
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solveMatrix) inv <<- solveMatrix
+  getInverse <- function() inv
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+}
+
+###2. Compute the matirx
+cacheSolve <- function(x, ...) {
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setInverse(inv)
+  inv      
+}
